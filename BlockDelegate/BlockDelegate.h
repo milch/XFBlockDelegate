6da88f562263dee79794e7134c3678981d0e53fe
Manu Wallner <manu.wallner@me.com>
2013-06-18 13:05:39 +0200

    Initial Commit

---
+++ BlockDelegate/BlockDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BlockDelegate.h
+//  BlockDelegate
+//
+//  Created by Manu Wallner on 18.06.13.
+//  Copyright (c) 2013 Xforge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockDelegate : NSObject
+
+@end
---
+++ BlockDelegateTests/BlockDelegateTests.h
@@ -0,0 +1,13 @@
+//
+//  BlockDelegateTests.h
+//  BlockDelegateTests
+//
+//  Created by Manu Wallner on 18.06.13.
+//  Copyright (c) 2013 Xforge. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BlockDelegateTests : SenTestCase
+
+@end
